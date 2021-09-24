5bbba3c149a55ed78eb98a46e60d3169789dab5f
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2021-09-23 20:42:29 -0500

    🅰 App created
    Angular app created
    Components
    header
    contact
    contact-list
    contact-detail

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactListComponent } from './contact/contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact/contact-detail/contact-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContactComponent,
+    ContactListComponent,
+    ContactDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/contact/contact.model.ts
@@ -0,0 +1,16 @@
+export class Contact {
+  public id: number;
+  public name: string;
+  public email: string;
+  public phone: string;
+  public imageUrl: string;
+  public group: any = null
+  constructor(id: number, name: string, email: string, phone: string, imageUrl: string, group: any) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.imageUrl = imageUrl;
+    this.group = group;
+  }
+}
