2b66bde0b4f5422b452358e7028108bb2488a618
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2021-10-05 23:16:59 -0500

    🔽 dropdown added

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentListComponent implements OnInit {
 
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+
+  documents = [
+    new Document(1, 'doc 1', 'desc 1', 'http://doc1.com'),
+    new Document(2, 'doc 2', 'desc 2', 'http://doc2.com'),
+    new Document(3, 'doc 3', 'desc 3', 'http://doc3.com'),
+    new Document(4, 'doc 4', 'desc 4', 'http://doc4.com')
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
